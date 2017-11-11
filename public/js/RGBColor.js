584e6aeacc103140cc992d6c2266ab14e9ce7cd2
Mark Armanious <markarmanus@gmail.com>
2017-11-11 16:43:33 +0000

    add comments

--- public/js/RGBColor.js
+++ public/js/RGBColor.js
@@ -17,9 +17,14 @@ function init(){
 	reset();
 }
 
+//function for seeting up the easy and hard buttons
 function setupModeButtons(){
+	//looping thourgh all items with class .mode
 	for(var i = 0; i < modeButtons.length; i++){
+		// adding an event listener
 		modeButtons[i].addEventListener("click", function(){
+
+			//removing class (selected from all elements and adding it to the one clicked			
 			modeButtons[0].classList.remove("selected");
 			modeButtons[1].classList.remove("selected");
 			this.classList.add("selected");
@@ -29,19 +34,27 @@ function setupModeButtons(){
 	}
 }
 
+
+
+//
 function setupSquares(){
 	for(var i = 0; i < squares.length; i++){
 	//add click listeners to squares
 		squares[i].addEventListener("click", function(){
 			//grab color of clicked square
 			var clickedColor = this.style.background;
+			
 			//compare color to pickedColor
 			if(clickedColor === pickedColor){
+				
+				//things to do when user wins
 				messageDisplay.textContent = "Correct!";
 				resetButton.textContent = "Play Again?"
 				changeColors(clickedColor);
 				h1.style.background = clickedColor;
 			} else {
+				
+				//thing to do when user loses 
 				this.style.background = "#232323";
 				messageDisplay.textContent = "Try Again"
 			}
@@ -51,14 +64,18 @@ function setupSquares(){
 
 
 
+// function to restart the game
 function reset(){
 	colors = generateRandomColors(numSquares);
+	
 	//pick a new random color from array
 	pickedColor = pickColor();
+	
 	//change colorDisplay to match picked Color
 	colorDisplay.textContent = pickedColor;
 	resetButton.textContent = "New Colors"
 	messageDisplay.textContent = "";
+	
 	//change colors of squares
 	for(var i = 0; i < squares.length; i++){
 		if(colors[i]){
@@ -68,13 +85,23 @@ function reset(){
 			squares[i].style.display = "none";
 		}
 	}
+	
+	
 	h1.style.background = "steelblue";
 }
+
+
+
+
+
 
 resetButton.addEventListener("click", function(){
 	reset();
 })
 
+
+
+//function to change colors when the user wins by passing the picked color
 function changeColors(color){
 	//loop through all squares
 	for(var i = 0; i < squares.length; i++){
@@ -83,11 +110,17 @@ function changeColors(color){
 	}
 }
 
+
+
+//function to randomly chose a color from the sqaures
 function pickColor(){
 	var random = Math.floor(Math.random() * colors.length);
 	return colors[random];
 }
 
+
+
+//function to generate a list of random colors with size num
 function generateRandomColors(num){
 	//make an array
 	var arr = []
@@ -100,6 +133,7 @@ function generateRandomColors(num){
 	return arr;
 }
 
+//function that will create 1 random rgb color
 function randomColor(){
 	//pick a "red" from 0 - 255
 	var r = Math.floor(Math.random() * 256);
